8da69f8914f6226de93e8a1771b2b171e6bd1dcd
Turtle-art123 <a8445@aevp.net>
2026-02-05 10:15:15 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,132 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "myImage1":return img`
+. . . . . . c c c c c c . . . . 
+. . . . c c b b d d d d c . . . 
+. . . c c b b d d d d d d c . . 
+. . c b b d b d d d d d d b c . 
+. c b b b d b b d d d d d b c . 
+. c b b b d d b d d d d b b c . 
+c b c b b b d d b b b b b c c . 
+c b c c b b b b d d d b c c c . 
+c b b c c c c c c c c c c c c . 
+c c b b b b b b c c b d d d b c 
+c c c c c c c c c d d d d d d c 
+c c c c c c c b c b d d d d d b 
+c b b b c c c b c c b d d d c b 
+c c b b b c c b b c c c c b b c 
+c c c c c c c c b b b b b b c c 
+c c c c c c c c c c c c c c c c 
+`;
+            case "image2":
+            case "myImage0":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . d 4 4 4 4 4 4 4 4 d . . . 
+. . . d 4 1 4 4 4 4 1 4 d . . . 
+. . . 4 4 f 4 4 4 4 f 4 4 . . . 
+. . . 4 4 4 4 d d 4 4 4 4 . . . 
+. . . 4 4 4 d d d d 4 4 4 . . . 
+. . . . 4 4 d 2 2 d 4 4 . . . . 
+. . . . . 4 d d d d 4 . . . . . 
+. . . . 6 6 d d d d 6 6 . . . . 
+. . . 6 6 6 6 6 6 6 6 6 6 . . . 
+. . . 4 4 6 6 6 6 6 6 4 4 . . . 
+. . . . . a a a a a a . . . . . 
+. . . . . a a . . a a . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . d 4 4 4 4 4 4 4 4 d . . . 
+. . . d 4 1 4 4 4 4 1 4 d . . . 
+. . . 4 4 f 4 4 4 4 f 4 4 . . . 
+. . . 4 4 4 4 d d 4 4 4 4 . . . 
+. . . 4 4 4 d d d d 4 4 4 . . . 
+. . . . 4 4 d 2 2 d 4 4 . . . . 
+. . . . . 4 d d d d 4 . . . . . 
+. . . . 6 6 d d d d 6 6 . . . . 
+. . . 6 6 6 6 6 6 6 6 6 6 . . . 
+. . . 4 4 6 6 6 6 6 6 4 4 . . . 
+. . . . . a a a a a a . . . . . 
+. . . . . f f . . a a . . . . . 
+. . . . . . . . . f f . . . . . 
+`, img`
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . d 4 4 4 4 4 4 4 4 d . . . 
+. . . d 4 1 4 4 4 4 1 4 d . . . 
+. . . 4 4 f 4 4 4 4 f 4 4 . . . 
+. . . 4 4 4 4 d d 4 4 4 4 . . . 
+. . . 4 4 4 d d d d 4 4 4 . . . 
+. . . . 4 4 d 2 2 d 4 4 . . . . 
+. . . . . 4 d d d d 4 . . . . . 
+. . . . 6 6 d d d d 6 6 . . . . 
+. . . 6 6 6 6 6 6 6 6 6 6 . . . 
+. . . 4 4 6 6 6 6 6 6 4 4 . . . 
+. . . . . a a a a a a . . . . . 
+. . . . . a a . . a a . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . d 4 4 4 4 4 4 4 4 d . . . 
+. . . d 4 1 4 4 4 4 1 4 d . . . 
+. . . 4 4 f 4 4 4 4 f 4 4 . . . 
+. . . 4 4 4 4 d d 4 4 4 4 . . . 
+. . . 4 4 4 d d d d 4 4 4 . . . 
+. . . . 4 4 d 2 2 d 4 4 . . . . 
+. . . . . 4 d d d d 4 . . . . . 
+. . . . 6 6 d d d d 6 6 . . . . 
+. . . 6 6 6 6 6 6 6 6 6 6 . . . 
+. . . 4 4 6 6 6 6 6 6 4 4 . . . 
+. . . . . a a a a a a . . . . . 
+. . . . . a a . . f f . . . . . 
+. . . . . f f . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
